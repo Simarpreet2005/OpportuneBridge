6f1136e6b928f1b6965817af991f3e3c3d1e4f14
Simarpreet2005 <simarsandhu0498@gmail.com>
2025-09-14 21:45:05 +0530

    added isAutenticated logic in middlewares in backend

---
+++ backend/middlewares/isAuthenticated.js
@@ -0,0 +1,27 @@
+import  jwt from "jsonwebtoken";
+
+const isAuthenticated=async(req,res,next)=>{
+    try{
+    const token=req.cookies.token;
+    if(!token){
+        return res.status(401).json({
+            message:"User not authenticated",
+            success:true 
+        })
+    }
+    const decode=await jwt.verify(token,process.env.SECRET_KEY);
+    if(!decode){
+        return res.status(401).json({
+            message:"Invalid token",
+            success:false
+        })
+    };
+    
+    }catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      message: "Server error",
+      success: false,
+    });
+  }
+}
