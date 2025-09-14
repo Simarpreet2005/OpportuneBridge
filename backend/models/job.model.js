8aaf4d4155eab62ec87818c7af0f5c38ae9c55dc
Simarpreet2005 <simarsandhu0498@gmail.com>
2025-09-14 18:28:22 +0530

    added company.model.js file

--- backend/models/job.model.js
+++ backend/models/job.model.js
@@ -40,9 +40,9 @@ const jobSchema=new mongoose.Schema({
     },
     applications:[
         {
-            type:mongoose.Schema.Types.ObjectID,
+            type:mongoose.Schema.Types.ObjectId,
             ref:'Application'
         }
     ]
-});
+},{timestamps:true});
 export const Job=mongoose.model("Job",jobSchema);
