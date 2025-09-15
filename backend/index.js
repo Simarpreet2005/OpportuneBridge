a1edf91bad3a21fa55bd2b2f1b6c885cbcfe8c23
Simarpreet2005 <simarsandhu0498@gmail.com>
2025-09-15 17:41:52 +0530

    updated api logic

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
 dotenv.config({});
-
+import userRoute from "./routes/user.route.js";
 
 const app=express();
 
@@ -20,6 +20,15 @@ const corsOptions={
 app.use(cors(corsOptions));
 
 const PORT=process.env.PORT || 3000;
+
+// API's
+
+app.use("/api/v1/user",userRoute);
+
+// "http://localhost:8000//api/v1/user/register"
+// "http://localhost:8000//api/v1/user/login"
+// "http://localhost:8000//api/v1/user/profile/update"
+
 
 app.listen(PORT,()=>{
     connectDB();
