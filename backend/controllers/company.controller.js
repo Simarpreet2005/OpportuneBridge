f14ae295f2b0b836af925adcb8aa55adefa9fb63
Simarpreet2005 <simarsandhu0498@gmail.com>
2025-09-18 21:08:01 +0530

    added register company function

---
+++ backend/controllers/company.controller.js
@@ -0,0 +1,30 @@
+import {Company} from "../models/company.model.js";
+
+export const registerCompany=async(req,res)=>{
+    try{
+      const{companyName}=req.body;
+      if(!companyName){
+        return res.status(400).json({
+        mesaage:"Company name is required",
+        success:false
+      });
+    }
+    let company=await company.findOne({name:companyName});
+    if(company){
+        return res.status(400).json({
+            message:"You can't register same company",
+            succes:false
+        })
+    };
+    company=await Company.create({
+    name:companyName,
+    userId:req.id
+    });
+    return res.status(201).json({
+        message:"Company registered successfully",
+        status:true
+    })
+}catch(error){
+        console.log(error);
+    }
+}
