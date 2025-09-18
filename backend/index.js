8425c23b1b17f4f42ed1cc86409e1eec1275a46f
Simarpreet2005 <simarsandhu0498@gmail.com>
2025-09-18 21:31:43 +0530

    added company.route.js file

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.route.js";
-
-
-// import companyRoute from "./routes/company.route.js";
+import companyRoute from "./routes/company.route.js";
 // import jobRoute from "./routes/job.route.js";
 // import applicationRoute from "./routes/application.route.js";
 
@@ -31,6 +29,7 @@ const PORT = process.env.PORT || 3000;
 // api's
 
 app.use("/api/v1/user", userRoute);
+app.use("/api/v1/company",companyRoute);
 
 // http://localhost:3000/api/v1/user/register
 // http://localhost:3000/api/v1/user/logout
