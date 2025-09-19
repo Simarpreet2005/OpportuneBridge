b2d04ea54a5d933a4a9ba151f541cdbf3337650c
Simarpreet2005 <simarsandhu0498@gmail.com>
2025-09-19 20:13:53 +0530

    added update status functionality

--- backend/controllers/application.controller.js
+++ backend/controllers/application.controller.js
@@ -1,6 +1,6 @@
 import { Job } from "../models/job.model";
 
-export const applyJob=async(requestAnimationFrame,res)=>{
+export const applyJob=async(req,res)=>{
     try{
     const userId=req.id;
     const jobId=req.params.id;
@@ -91,6 +91,35 @@ export const getApplicants=async(req,res)=>{
         })
     }
         catch(error){
+            console.log(error); 
+    }
+}
+export const updateStatus=async(req,res)=>{
+    try{
+        const {status}=req.body;
+        const applicationId=req.params.id;
+        if(!status){
+            return res.status(400).json({
+                message:"Status is required",
+                success:false
+            })
+        }
+        // find the application by applicantion id
+        const application=await Application.findOne({_id:applicationId})
+        if(!application){
+            return res.status(404).json({
+                message:"Application not found",
+                success:false
+            })
+        }
+        // update the status
+        application.status=status.toLowerCase();
+        await application.save();
+        return res.status(200).json({
+            message:"Application status updated successfully",
+            success:true
+        })  
+    }    catch(error){
             console.log(error); 
     }
 }
