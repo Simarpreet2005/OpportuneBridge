bd519b54c97c3b5c38166e88b53f66f8fd676f51
Simarpreet2005 <simarsandhu0498@gmail.com>
2025-09-19 16:57:57 +0530

    added get all jobs functionality

--- backend/controllers/job.controller.js
+++ backend/controllers/job.controller.js
@@ -1,3 +1,5 @@
+import { Job } from "../models/job.model";
+
 export const postJob =async(req,res)=>{
 try{
 const{title,description,requirements,salary,location,jobType,experience,position,companyId}=req.body;
@@ -18,7 +20,7 @@ const job=await Job.create({
   company:companyId,
   created_by:userId  
 });
-return res.status(200).json({
+return res.status(201).json({
     message:"New job created Successfully",
     job,
     success:true
@@ -26,4 +28,29 @@ return res.status(200).json({
 }catch(error){
     console.log(error);
 }
+}
+export const getAllJobs=async(req,res)=>{
+    try{
+        const keyword=req.query.keyword || "";
+        const query={
+            $or:[
+               {title:{$regex:keyword,$options:"i"}},
+               {description:{$regex:keyword,$options:"i"}} 
+            ]
+        };
+        const jobs=await Job.find(query);
+        if(!jobs){
+    return res.status(404).json({
+        message:"Job not found",
+        success:false
+})
+        }
+return res.status(201).json({
+    jobs,
+    success:true
+});
+
+    }catch(error){
+        console.log(error);
+    }
 }
