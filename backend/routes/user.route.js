308822c9a70d53f8f56c5410829cdd53f128a173
Simarpreet2005 <simarsandhu0498@gmail.com>
2025-10-08 21:13:41 +0530

    added Footer.jsx

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -3,7 +3,7 @@ import {login,register,updateProfile,logout} from "../controllers/user.controlle
 import isAuthenticated from "../middlewares/isAuthenticated.js";
 const router=express.Router();
 
-router.route("/register").post(register);
+router.route("/register").post(singleUpload,register);
 router.route("/login").post(login);
 router.route("/logout").get(logout);
 router.route("/profile/update").post(isAuthenticated, updateProfile);
